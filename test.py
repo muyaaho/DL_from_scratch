9acdbfdcf94bab572d450a42d52aabb6d21df5a4
muyaaho <skanthsu715@gmail.com>
2023-04-05 13:47:33 +0900

    :rotating_light: 패키지 테스트

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+# from dezero.core_simple import Variable
+
+# x = Variable(np.array(1.0))
+# print(x)
+
+from dezero import Variable
+x = Variable(np.array(1.0))
+print(x)
+# variable(1.0)
