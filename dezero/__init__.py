2ca3626d8303c7d5776baf687d0327320c755ea1
muyaaho <skanthsu715@gmail.com>
2023-04-05 13:47:16 +0900

    :sparkles: 패키지로 정리

---
+++ dezero/__init__.py
@@ -0,0 +1,17 @@
+'''
+모듈 임포트 할 때 가장 먼저 실행됨
+'''
+is_simple_core = True
+
+if is_simple_core:
+    from dezero.core_simple import Variable
+    from dezero.core_simple import Function
+    from dezero.core_simple import using_config
+    from dezero.core_simple import no_grad
+    from dezero.core_simple import as_array
+    from dezero.core_simple import as_variable
+    from dezero.core_simple import setup_variable
+
+
+
+setup_variable()
