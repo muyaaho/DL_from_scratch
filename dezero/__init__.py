51e54730c23d3f6f5732dc6b054e4057303cab98
muyaaho <skanthsu715@gmail.com>
2023-04-11 02:42:53 +0900

    :sparkles: Add dezero.core import

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,7 +1,7 @@
 '''
 모듈 임포트 할 때 가장 먼저 실행됨
 '''
-is_simple_core = True
+is_simple_core = False
 
 if is_simple_core:
     from dezero.core_simple import Variable
@@ -11,7 +11,14 @@ if is_simple_core:
     from dezero.core_simple import as_array
     from dezero.core_simple import as_variable
     from dezero.core_simple import setup_variable
-
+else:
+    from dezero.core import Variable
+    from dezero.core import Function
+    from dezero.core import using_config
+    from dezero.core import no_grad
+    from dezero.core import as_array
+    from dezero.core import as_variable
+    from dezero.core import setup_variable
 
 
 setup_variable()
